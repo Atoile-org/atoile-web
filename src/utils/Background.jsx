536115cab3638d1a7f2c8d6d0acfeb5ad5fbc489
tbvns <tbvns601@gmail.com>
2026-07-12 18:34:07 +0200

    Changed the BG video to include motion blur and let the server handle the video to allow for proper caching.

--- src/utils/Background.jsx
+++ src/utils/Background.jsx
@@ -1,11 +1,10 @@
 import './Background.css'
-import background from '../assets/background.mp4'
 
 export default function Background({ children }) {
   return (
     <section className="hero-video">
       <video className="hero-video_bg" autoPlay muted loop playsInline>
-        <source src={background} type="video/mp4" />
+        <source src="/bg.mp4" type="video/mp4" />
       </video>
 
       <div className="hero-video_content">
