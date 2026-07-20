e6f08ef840973ac6ab69886a988bb961f44e881a
dinarudimdams <dinaru@dimdams.com>
2026-07-20 19:30:51 +0200

    Added my base member info

--- src/pages/Members/Members.jsx
+++ src/pages/Members/Members.jsx
@@ -8,72 +8,78 @@ import TiktokIcon from "../../assets/icons/tiktok.svg?react"
 import RedditIcon from '@mui/icons-material/Reddit';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import XIcon from '@mui/icons-material/X';
+import TelegramIcon from '@mui/icons-material/Telegram';
 import TwitchIcon from "../../assets/icons/twitch.svg?react"
 import DiscordIcon from "../../assets/icons/discord.svg?react"
 
 import tbvns from "./member/tbvns/tbvns.json"
 import tbvnsShortEn from "./member/tbvns/short/en.md?raw"
 import tbvnsShortFr from "./member/tbvns/short/fr.md?raw"
+import dinaru from "./member/dinaru/dinaru.json"
+import dinaruShortEn from "./member/dinaru/short/en.md?raw"
+import dinaruShortFr from "./member/dinaru/short/fr.md?raw"
 
-function Member({ member }) {
-    const {t} = useTranslation();
-    return (
-        <div className={"member"} >
-            <div>
-                <img src={member.img1} className={"memberImgLeft"} />
-                <h2>{t("global.langId") === "fr" ? member.name.fr : member.name.en}</h2>
-                <div className={"member-short"}>
-                    <ReactMarkdown>{t("global.langId") === "fr" ? tbvnsShortFr : tbvnsShortEn}</ReactMarkdown>
-                </div>
-                <div className={"member-info"}>
-                    <div className={"member-info-box"}>
-                        <div className={"member-text-content"}>
-                            <div className={"member-social"}>
-                                <h2>Socials:</h2>
-                                {member.socials.web && <div className={"member-social-icons"}>
-                                    <a href={member.socials.web} ><LanguageIcon /></a>
-                                </div>}
-                                {member.socials.youtube && <div className={"member-social-icons"}>
-                                    <a href={member.socials.youtube} ><YouTubeIcon /></a>
-                                </div>}
-                                {member.socials.tiktok && <div className={"member-social-icons"}>
-                                    <a href={member.socials.tiktok} ><TiktokIcon /></a>
-                                </div>}
-                                {member.socials.reddit && <div className={"member-social-icons"}>
-                                    <a href={member.socials.reddit} ><RedditIcon /></a>
-                                </div>}
-                                {member.socials.github && <div className={"member-social-icons"}>
-                                    <a href={member.socials.github} ><GitHubIcon /></a>
-                                </div>}
-                                {member.socials.x && <div className={"member-social-icons"}>
-                                    <a href={member.socials.x} ><XIcon /></a>
-                                </div>}
-                                {member.socials.twitch && <div className={"member-social-icons"}>
-                                    <a href={member.socials.twitch} ><TwitchIcon /></a>
-                                </div>}
-                                {member.socials.discord && <div className={"member-social-icons"}>
-                                    <a href={member.socials.discord} ><DiscordIcon /></a>
-                                </div>}
-                            </div>
-                            <h2>Role: <span className={"text-highlight"}>{t("global.langId") === "fr" ? member.status.fr : member.status.en}</span></h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
+function Member({ member, shortFr, shortEn }) {
+  const {t} = useTranslation();
+  return (
+    <div className={"member"} >
+      <div>
+        <img src={member.img1} className={"memberImgLeft"} />
+        <h2>{t("global.langId") === "fr" ? member.name.fr : member.name.en}</h2>
+        <div className={"member-short"}>
+          <ReactMarkdown>{t("global.langId") === "fr" ? shortFr : shortEn}</ReactMarkdown>
         </div>
-    )
+        <div className={"member-info"}>
+          <div className={"member-info-box"}>
+            <div className={"member-text-content"}>
+              <div className={"member-social"}>
+                <h2>Socials:</h2>
+                {member.socials.web && <div className={"member-social-icons"}>
+                  <a href={member.socials.web} ><LanguageIcon /></a>
+                </div>}
+                {member.socials.youtube && <div className={"member-social-icons"}>
+                  <a href={member.socials.youtube} ><YouTubeIcon /></a>
+                </div>}
+                {member.socials.telegram && <div className={"member-social-icons"}>
+                  <a href={member.socials.telegram} ><TelegramIcon /></a>
+                </div>}
+                {member.socials.tiktok && <div className={"member-social-icons"}>
+                  <a href={member.socials.tiktok} ><TiktokIcon /></a>
+                </div>}
+                {member.socials.reddit && <div className={"member-social-icons"}>
+                  <a href={member.socials.reddit} ><RedditIcon /></a>
+                </div>}
+                {member.socials.github && <div className={"member-social-icons"}>
+                  <a href={member.socials.github} ><GitHubIcon /></a>
+                </div>}
+                {member.socials.x && <div className={"member-social-icons"}>
+                  <a href={member.socials.x} ><XIcon /></a>
+                </div>}
+                {member.socials.twitch && <div className={"member-social-icons"}>
+                  <a href={member.socials.twitch} ><TwitchIcon /></a>
+                </div>}
+                {member.socials.discord && <div className={"member-social-icons"}>
+                  <a href={member.socials.discord} ><DiscordIcon /></a>
+                </div>}
+              </div>
+              <h2>Role: <span className={"text-highlight"}>{t("global.langId") === "fr" ? member.status.fr : member.status.en}</span></h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default function Members({ route }) {
-    const { t } = useTranslation()
-    return (
-        <Background fixed>
-            <div className="members-root">
-                <h1>Our Members</h1>
-                <Member
-                    member={tbvns}
-                />
-            </div>
-        </Background>
-    )
+export default function Members() {
+  const {t} = useTranslation()
+  return (
+    <Background fixed>
+      <div className="members-root">
+        <h1>{t("members.our-members")}</h1>
+        <Member member={tbvns} shortFr={tbvnsShortFr} shortEn={tbvnsShortEn} />
+        <Member member={dinaru} shortFr={dinaruShortFr} shortEn={dinaruShortEn} />
+      </div>
+    </Background>
+  )
 }
---
+++ src/pages/Members/member/dinaru/short/fr.md
@@ -0,0 +1,2 @@
+Je m'appelle Gwendal, de pseudonyme dinarudimdams. Je suis actuellement en terminal et je côtoie le monde du développement depuis mes 13ans. Également grand utilisateur d'Arch Linux, je suis co-fondateur d'ATOILE.
+
